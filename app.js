d8251445aec2ace616e28733128999fa18792af5
Lizbeth-Laptop <115133403+WillAvatec@users.noreply.github.com>
2023-12-03 12:13:08 -0500

    Send data as params

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const path = require("path");
 const ytdl = require("ytdl-core");
 const ProgressBar = require("progress")
 const morgan = require("morgan")
+const bodyParser = require("body-parser")
 
 const app = express()
 
 const URL = "https://www.youtube.com/watch?v=1uPBzLkDYLI"
 
-app.use(morgan())
+app.use(morgan("dev"))
+app.use(bodyParser.json())
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"index.html"))
@@ -19,8 +21,6 @@ app.get("/",(req,res)=>{
 
 let bar;
 app.get("/video",(req,res)=>{
-    console.log("Request on /video")
-
     const stream = ytdl(URL, {
         filter:"audioandvideo",
         quality:"highestvideo",
@@ -40,6 +40,32 @@ app.get("/video",(req,res)=>{
         res.end()
     })
     
+    stream.pipe(res)
+})
+ 
+app.post("/video/:id/:format",async (req,res)=>{
+    console.log(req.params)
+    const {id, format } = req.params;
+
+    const info = await ytdl.getBasicInfo(id);
+    const length = info.formats[0].contentLength
+    const title = info.videoDetails.title;
+    const fileName = `${title}.${format === "audio" ? "mp3" : "mp4"}`;
+    const contentType = format === "audio" ? "audio/mpeg" : "video/mp4"; 
+
+    res.setHeader(
+        "Content-Type", contentType
+    )
+    res.setHeader(
+        "Content-Disposition", `attachment; filename=${fileName}`
+    )
+
+
+    const stream = ytdl(id,{
+        quality:"highestvideo",
+        filter: format === "audio" ? "audioonly" : "audioandvideo"
+    })
+
     stream.pipe(res)
 })
 
